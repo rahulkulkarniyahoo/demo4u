c69d7674440aa8c472fcff1f4ef580444e5bdb6c
Rahul Kulkarni <Rahul.Kulkarni@mailnewspapers.co.uk>
2015-10-26 11:19:53 +0000

    My Demo message added

--- DemoMonday/src/demopkg/DemoClass1.java
+++ DemoMonday/src/demopkg/DemoClass1.java
@@ -4,7 +4,7 @@ public class DemoClass1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Monday");
-
+		System.out.println("This is part of Demo/Feature/F1. Hope i can see it");
 	}
 
 }
